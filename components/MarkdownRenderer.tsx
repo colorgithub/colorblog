91c076b84346b006e2028a4678400831d637ba03
color <color@github.com>
2026-07-28 19:16:18 +0800

    Add remark-breaks plugin to preserve single newlines

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import remarkBreaks from 'remark-breaks'
 import rehypeSlug from 'rehype-slug'
 import rehypeHighlight from 'rehype-highlight'
 
@@ -7,7 +8,7 @@ export function MarkdownRenderer({ content }: { content: string }) {
   return (
     <div className="prose prose-lg max-w-none dark:prose-invert">
       <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
+        remarkPlugins={[remarkGfm, remarkBreaks]}
         rehypePlugins={[rehypeSlug, rehypeHighlight]}
       >
         {content}
